e05bef7b6a99ce54a8362692a418e28e700af25e
Sam130687 <sam130687@gmail.com>
2023-11-24 12:36:47 +0300

    fix part of homework

--- src/components/counter/component.jsx
+++ src/components/counter/component.jsx
@@ -1,12 +1,18 @@
+import { useEffect, useState } from "react"
+
 export const Counter = ({count=0, step=1, onCounterClick, min=0, max=5}) => {
+    const [amount, setAmount] = useState(count);
+
+    useEffect(()=>{onCounterClick(amount)}, [amount]);
+
     return (
         <div>
             <button onClick={
-                () => onCounterClick(count > min ? count - step : 0 )
+                () => setAmount(count > min ? count - step : 0 )
             }>-</button>
             <span>{count}</span>
             <button onClick={
-                () => onCounterClick(count < max ? count + step : 5 )
+                () => setAmount(count < max ? count + step : 5 )
             }>+</button>
         </div>
     )
